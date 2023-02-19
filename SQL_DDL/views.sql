c1b4c4bb467681000e31efff01cf486ab60b3469
David Hoffmann <david.hoffmann@mail.de>
2023-02-19 21:25:12 +0100

    New view

--- SQL_DDL/views.sql
+++ SQL_DDL/views.sql
@@ -6,19 +6,19 @@ LEFT JOIN airlinex_employee ON airportx_airport.icao_code = airlinex_employee.ba
 GROUP BY airportx_airport.icao_code;
 
 -- View: incomming flights for each airport
-CREATE OR REPLACE VIEW airport_stats AS
-SELECT 
-  airportx_airport.icao_code, 
-  AVG(airlinex_flight.delay) AS average_delay, 
-  COUNT(airlinex_flight.number) AS number_flights, 
-  COUNT(airlinex_booking.id) AS number_bookings 
-FROM 
-  airportx_airport 
-  JOIN airlinex_flight 
-  ON airportx_airport.icao_code = airlinex_flight.departure_airport_id 
-  OR airportx_airport.icao_code = airlinex_flight.destination_airport_id
-GROUP BY 
-  airportx_airport.icao_code;
+-- CREATE OR REPLACE VIEW airport_stats AS
+-- SELECT 
+--   airportx_airport.icao_code, 
+--   AVG(airlinex_flight.delay) AS average_delay, 
+--   COUNT(airlinex_flight.number) AS number_flights, 
+--   COUNT(airlinex_booking.id) AS number_bookings 
+-- FROM 
+--   airportx_airport 
+--   JOIN airlinex_flight 
+--   ON airportx_airport.icao_code = airlinex_flight.departure_airport_id 
+--   OR airportx_airport.icao_code = airlinex_flight.destination_airport_id
+-- GROUP BY 
+--   airportx_airport.icao_code;
 
 -- View: incomming flights for each airport
 CREATE OR REPLACE VIEW airport_stats AS
