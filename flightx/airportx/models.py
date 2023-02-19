27f9d3a9ae65cd55b0aeba08b68b5367b816f1c5
David Hoffmann <david.hoffmann@mail.de>
2023-02-19 20:31:20 +0100

    Finished View

--- flightx/airportx/models.py
+++ flightx/airportx/models.py
@@ -25,13 +25,12 @@ class AirportStats(models.Model):
     icao_code = models.CharField("Unique ICAO airport code", max_length=4, primary_key=True)
     average_delay = models.FloatField("Average Delay")
     number_flights = models.IntegerField("Number of Flights")
-    number_bookings = models.IntegerField("Number of Passengers")
 
     class Meta:
         managed = False
         db_table = 'airport_stats'
 
     def __str__(self):
-        return f"ICAO: {self.icao_code}, Delay: {self.average_delay}, Flights: {self.number_flights}, Bookings: {self.number_bookings}"
+        return f"ICAO: {self.icao_code}, Delay: {self.average_delay}, Flights: {self.number_flights}"
 
 
